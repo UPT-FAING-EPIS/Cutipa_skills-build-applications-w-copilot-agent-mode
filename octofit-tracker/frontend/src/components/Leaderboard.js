ca02ae8e2720242416498534d1843a27d95f93e3
RicardoCutipa <rc2021069827@virtual.upt.pe>
2026-04-26 00:45:55 -0500

    Add frontend API endpoint placeholders

---
+++ octofit-tracker/frontend/src/components/Leaderboard.js
@@ -0,0 +1,1 @@
+const leaderboardEndpoint = "https://${REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/leaderboard/";
---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,1 @@
+const teamsEndpoint = "https://${REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/teams/";
